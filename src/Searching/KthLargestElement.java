e9f6843109554c53f294c71b30510165c8ae968d
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-02 16:11:00 +0530

    kth largets lemenent

---
+++ src/Searching/KthLargestElement.java
@@ -0,0 +1,22 @@
+package Searching;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            pq.add(nums[i]);
+        }
+        for(int i=k;i<nums.length;i++){
+            if(pq.peek()<nums[i]){
+                pq.poll();
+                pq.add(nums[i]);
+            }
+        }
+        System.out.println(pq.peek());
+    }
+}
