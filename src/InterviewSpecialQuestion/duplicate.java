fe767aca2eba95b69d381a16d307da2b3de40fbd
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-04 11:28:31 +0530

    factorail

--- src/InterviewSpecialQuestion/duplicate.java
+++ src/InterviewSpecialQuestion/duplicate.java
@@ -1,4 +1,15 @@
 package InterviewSpecialQuestion;
 
 public class duplicate {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,2,5,1,6,3};
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println("duplicate element is: "+arr[i]);
+                }
+            }
+        }
+
+    }
 }
