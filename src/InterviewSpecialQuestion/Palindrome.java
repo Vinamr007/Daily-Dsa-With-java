1a15217411170f3a2e6aa876bd2eed80df570143
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-04 11:16:59 +0530

    palindrome

---
+++ src/InterviewSpecialQuestion/Palindrome.java
@@ -0,0 +1,21 @@
+package InterviewSpecialQuestion;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        int v=12321;
+        int v1=0;
+
+        while(v>0){
+            int digit=v%10;
+            v1=v1*10+digit;
+        v=v/10;
+
+        }
+        if(v1==v){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not a palindrome");
+        }
+   }
+}
