1ebb0c492fe09eb13d37678a23f7d1861fc3432e
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-04 11:35:36 +0530

    a

---
+++ src/InterviewSpecialQuestion/Amstrong.java
@@ -0,0 +1,19 @@
+package InterviewSpecialQuestion;
+
+public class Amstrong {
+    public static void main(String[] args) {
+        int n = 153;
+        int n1, rev = 0, sum = 0;
+        n1 = n;
+        while (n > 0) {
+            rev = n % 10;
+            sum = sum + (rev * rev * rev);
+            n = n / 10;
+        }
+        if (sum == n1) {
+            System.out.println("amstrong number");
+        } else {
+            System.out.println("not a amstrong number");
+        }
+    }
+}
