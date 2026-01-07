a4befc615abbea9d96bef617d02877aab70678f4
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-07 11:27:11 +0530

    reverse array

---
+++ src/practice/reversearray.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class reversearray {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8,9,10};
+        int st=0,end=arr.length-1;
+        while(st<end){
+            int temp=arr[st];
+            arr[st]=arr[end];
+            arr[end]=temp;
+            st ++;
+            end --;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
