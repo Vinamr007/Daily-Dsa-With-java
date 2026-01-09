037211a98e91446e5336bcbead4785c343a66f52
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-09 11:52:18 +0530

    pattern

---
+++ src/InterviewSpecialQuestion/Starpattern/LeftHalfPyramid.java
@@ -0,0 +1,17 @@
+package InterviewSpecialQuestion.Starpattern;
+
+public class LeftHalfPyramid
+{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
