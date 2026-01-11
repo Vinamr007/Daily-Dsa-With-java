314449ca0665aa16b8dd1f9ad03ca2577ceba456
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-11 10:33:22 +0530

    insertin sort

---
+++ src/Sorting/insertionsort.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+public class insertionsort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements that are greater than key
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+
+        insertionSort(arr);
+
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
