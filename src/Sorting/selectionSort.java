baec420b48d6f1904e71cfb05978dc1fa6bc84fd
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-13 23:39:59 +0530

    selection sort

---
+++ src/Sorting/selectionSort.java
@@ -0,0 +1,32 @@
+public class selectionSort {
+    public static void swap(int[] arr, int min, int j){
+        int temp=arr[min];
+        arr[min]=arr[j];
+        arr[j]=temp;
+    }
+    public static void main(String[] args) {
+        int[] arr={64, 25, 12, 22, 11};
+ for(int i=0;i<arr.length-1;i++){
+    int min=i;
+
+    for(int j=i+1;j<arr.length;j++){
+if(arr[j]<arr[min]){
+    min=j;
+}
+
+
+    }
+
+if(min!=i){
+    swap(arr, min, i);
+}
+
+
+
+ }
+    for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+    }
+    }
+    
+}
