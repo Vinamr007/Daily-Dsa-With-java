ca74f04570fc5b9a088255116fab0e24137c16e1
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-13 06:28:17 +0530

    reverse string

--- src/InterviewSpecialQuestion/ReversingString.java
+++ src/InterviewSpecialQuestion/ReversingString.java
@@ -2,11 +2,12 @@ package InterviewSpecialQuestion;
 
 public class ReversingString {
     public static void main(String[] args) {
-        String s="vinu";
-        String r="";
-        for(int i=0;i<s.length();i++){
-            r=s.charAt(i)+r;
+        String str="vinu";
+
+        String rev="";
+        for(int i=str.length()-1;i>=0;i--) {
+            rev = rev + str.charAt(i);
         }
-        System.out.println(r);
+        System.out.println(rev);
     }
 }
