2c76489e399c5b61ac58af7b8c2af906bfcc72c7
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-14 23:02:19 +0530

    prime number oprtion code

--- src/InterviewSpecialQuestion/IsPrime.java
+++ src/InterviewSpecialQuestion/IsPrime.java
@@ -1,29 +1,47 @@
 package InterviewSpecialQuestion;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class IsPrime {
     public static void main(String[] args) {
-boolean isprime[]=seiveOfEratosthenes(20);
-for(int i=0;i<20;i++){
-    if(isprime[i]==true){
-    System.out.println(i+" "+isprime[i]);
-}}
-    }
-    static boolean[] seiveOfEratosthenes(int n){
-        boolean[] isprime=new boolean[n+1];
-        Arrays.fill(isprime,true);
-        isprime[0]=false;
-        isprime[1]=false;
-        for(int i=2;i*i<=n;i++){
-            if(isprime[i]){
-                for(int j=i*i;j<=n;j+=i){
-                    isprime[j]=false;
-                }
+//boolean isprime[]=seiveOfEratosthenes(20);
+//for(int i=0;i<20;i++){
+//    if(isprime[i]==true){
+//    System.out.println(i+" "+isprime[i]);
+//}}
+//    }
+//    static boolean[] seiveOfEratosthenes(int n){
+//        boolean[] isprime=new boolean[n+1];
+//        Arrays.fill(isprime,true);
+//        isprime[0]=false;
+//        isprime[1]=false;
+//        for(int i=2;i*i<=n;i++){
+//            if(isprime[i]){
+//                for(int j=i*i;j<=n;j+=i){
+//                    isprime[j]=false;
+//                }
+//            }
+//        }
+//
+//        return isprime;
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to check if it is prime:");
+        int number=sc.nextInt();
+        int count=0;
+        for(int i=2;i<number;i++){
+            if(number%i==0){
+                count++;
+                break;
             }
         }
+        if(count==0){
+            System.out.println(number+" is a prime number.");
+        }else {
+            System.out.println(number + " is not a prime number.");
+        }
 
-        return isprime;
     }
 
 }
