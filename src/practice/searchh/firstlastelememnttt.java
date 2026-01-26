4911a80e0ff1dab1db7b51c366d9bb8273522eb6
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-26 10:03:36 +0530

    modified code

--- src/practice/searchh/firstlastelememnttt.java
+++ src/practice/searchh/firstlastelememnttt.java
@@ -5,6 +5,27 @@ public class firstlastelememnttt {
         int[] arr={1,2,2,2,2,3,4,5};
         int target=2;
 
+        // first occurance
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                System.out.println("First occurence of "+target+" is at index: "+i);
+                break;
+            }
+        }
+        //last occurance
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]==target){
+                System.out.println("Last occurence of "+target+" is at index: "+i);
+                break;
+            }
+        }
+
+
+
+
+
+
+
 //        //first occurence
 //        int first=-1;
 //        int start=0;
