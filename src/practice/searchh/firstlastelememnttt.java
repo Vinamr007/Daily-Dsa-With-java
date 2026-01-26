1d786846b205ea7b5a02223903adc39cd7d89911
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-26 09:58:51 +0530

    first last occurance

---
+++ src/practice/searchh/firstlastelememnttt.java
@@ -0,0 +1,46 @@
+package practice.searchh;
+
+public class firstlastelememnttt {
+    public static void main(String[] args) {
+        int[] arr={1,2,2,2,2,3,4,5};
+        int target=2;
+
+//        //first occurence
+//        int first=-1;
+//        int start=0;
+//        int end=arr.length-1;
+//        while(start<=end){
+//            int mid=start+(end-start)/2;
+//            if(arr[mid]==target){
+//                first=mid;
+//                end=mid-1;
+//            }
+//            else if(target>arr[mid]){
+//                start=mid+1;
+//            }
+//            else{
+//                end=mid-1;
+//            }
+//        }
+//        System.out.println("First occurence of "+target+" is at index: "+first);
+//
+//        //last occurence
+//        int last=-1;
+//         start=0;
+//         end=arr.length-1;
+//        while(start<=end){
+//            int mid=start+(end-start)/2;
+//            if(arr[mid]==target){
+//                last=mid;
+//                start=mid+1;
+//            }
+//            else if(target>arr[mid]){
+//                start=mid+1;
+//            }
+//            else{
+//                end=mid-1;
+//            }
+//        }
+//        System.out.println("Last occurence of "+target+" is at index: "+last);
+    }
+}
