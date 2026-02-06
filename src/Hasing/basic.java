0fd0465d446ec907ff12ef3af147f2ab5ddec61a
Vinamr Naik <vinamrnaik206@gmail.com>
2026-02-06 11:43:58 +0530

    all operation of hashing

--- src/Hasing/basic.java
+++ src/Hasing/basic.java
@@ -4,12 +4,21 @@ import java.util.HashSet;
 
 public class basic {
     public static void main(String[] args) {
-        HashSet<Integer> s=new HashSet<>();
+        HashSet<Integer> s = new HashSet<>();
         s.add(1);
         s.add(2);
         s.add(3);
 
         System.out.println(s);
+        if (s.contains(2)) {
+            System.out.println("set contains 2");
+        } else {
+            System.out.println("set not contains 2");
+        }
+ s.remove(2);
+        System.out.println(s.isEmpty());
+        System.out.println(s.size());
+s.clear();
 
     }
 }
