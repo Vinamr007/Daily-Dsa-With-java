5d364c8af8b0e574f81ffb4f447f5973dd8d06ba
Vinamr Naik <vinamrnaik206@gmail.com>
2026-02-13 17:13:35 +0530

    trying code

---
+++ src/Hasing/Longestsequence.java
@@ -0,0 +1,24 @@
+package Hasing;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Longestsequence {
+    public static void longestConsecutive(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int[]  nums = {100, 4, 200, 1, 3, 2};
+        Arrays.sort(nums);
+    }
+}
