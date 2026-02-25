47eeb0261a0d4a7b14ce05e1057633294ca77a9f
Vinamr Naik <vinamrnaik206@gmail.com>
2026-02-25 11:32:48 +0530

    all problem solution

--- src/TCs/Arraya/sumarr.java
+++ src/TCs/Arraya/sumarr.java
@@ -2,14 +2,59 @@ package TCs.Arraya;
 
 public class sumarr {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,50};
+        int[] arr={1,2,3,4,50,50};
         int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
+        //for sumofarray
+//        for(int i=0;i<arr.length;i++){
+//            sum+=arr[i];
+//
+//        }
+//        System.out.println(sum);
 
-        }
+
+        //for Mean of array
+
+//        for(int i=0;i<arr.length;i++){
+//            sum+=arr[i];
+//
+//        }
+//
+//
+//        int mean=sum/arr.length;
+//        System.out.println(mean);
 
 
-        System.out.println(sum);
+        // Reverse of array
+//        int st=0,end=arr.length-1;
+//        while(st<end){
+//            int temp=arr[st];
+//            arr[st]=arr[end];
+//            arr[end]=temp;
+//            st++;
+//            end--;
+//        }
+//        for(int num :arr){
+//            System.out.print(num+" ");
+//        }
+
+// removing duplicate from sorted array
+        //   int[] arr={1,2,3,4,50};
+//
+//        int[] arr1=new int[arr.length];
+//        int j=0;
+//
+//        for(int i=0;i<arr.length-1;i++){
+//            if(arr[i]!=arr[i+1]){
+//                arr1[j++]=arr[i];
+//            }
+//        }
+//        arr1[j++]=arr[arr.length-1];
+//
+//
+//        for(int i=0;i<j;i++){
+//            System.out.print(arr1[i]+" ");
+//        }
+
+
     }
 }
