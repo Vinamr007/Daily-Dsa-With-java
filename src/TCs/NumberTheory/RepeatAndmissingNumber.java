93afebacef8cacd48668cbf7c2dde5ca1c1fbc53
Vinamr Naik <vinamrnaik206@gmail.com>
2026-03-11 10:19:49 +0530

    missing and repeating number

---
+++ src/TCs/NumberTheory/RepeatAndmissingNumber.java
@@ -0,0 +1,21 @@
+package TCs.NumberTheory;
+
+public class RepeatAndmissingNumber {
+    public static void main(String[] args) {
+        int[] arr={4,5,6,2,1,1};
+        int n=arr.length;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+        }
+        int expectedsum=n*(n+1)/2;
+        int c=expectedsum-sum;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]==arr[i+1]){
+                System.out.println(arr[i]+" "+(c+arr[i]));
+            }
+        }
+
+    }
+}
