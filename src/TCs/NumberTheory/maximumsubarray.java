f7cac25ee1dacd91fa77815f5a690036d8b3e715
Vinamr Naik <vinamrnaik206@gmail.com>
2026-04-01 18:01:45 +0530

    commenting

--- src/TCs/NumberTheory/maximumsubarray.java
+++ src/TCs/NumberTheory/maximumsubarray.java
@@ -2,21 +2,21 @@ package TCs.NumberTheory;
 
 public class maximumsubarray {
     public static void main(String[] args) {
-        int[] arr={-2,-4};
-
-        int maxsum=arr[0];
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-
-               sum+=arr[i];
-                if(sum>maxsum){
-                    maxsum=sum;
-                }
-                if(sum<0)
-                {
-                    sum=0;
-                }
-        }
-        System.out.println(maxsum);
+//        int[] arr={-2,-4};
+//
+//        int maxsum=arr[0];
+//        int sum=0;
+//        for(int i=0;i<arr.length;i++){
+//
+//               sum+=arr[i];
+//                if(sum>maxsum){
+//                    maxsum=sum;
+//                }
+//                if(sum<0)
+//                {
+//                    sum=0;
+//                }
+//        }
+//        System.out.println(maxsum);
      }
 }
