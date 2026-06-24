523062c8649205c3aa06134daa7f6b85f56c04f6
Vinamr Naik <vinamrnaik206@gmail.com>
2026-06-24 22:53:17 +0530

    even odd number

--- src/DSA_BOOTCAMP/firstprogram.java
+++ src/DSA_BOOTCAMP/firstprogram.java
@@ -1,7 +1,16 @@
 package DSA_BOOTCAMP;
 
+import java.util.Scanner;
+
 public class firstprogram {
     public static void main(String[] args) {
-
+Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
+       int n=sc.nextInt();
+       if(n%2==0){
+           System.out.println("Even");
+       } else{
+           System.out.println("Odd");
+       }
     }
 }
