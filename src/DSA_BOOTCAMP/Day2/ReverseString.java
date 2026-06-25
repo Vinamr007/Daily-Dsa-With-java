76f51afc5e3001a5cbdcfe442fb36a9a7b26e786
Vinamr Naik <vinamrnaik206@gmail.com>
2026-06-26 01:12:21 +0530

    palindrome string & reverse string

---
+++ src/DSA_BOOTCAMP/Day2/ReverseString.java
@@ -0,0 +1,12 @@
+package DSA_BOOTCAMP.Day2;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String s="vinu";
+        String r="";
+        for(int i=0;i<s.length();i++){
+            r=s.charAt(i)+r;
+        }
+        System.out.println(r);
+    }
+}
