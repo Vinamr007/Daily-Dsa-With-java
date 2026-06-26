35cf29f63b0d7088103c80dc0c3c033a8c1d3450
Vinamr Naik <vinamrnaik206@gmail.com>
2026-06-27 00:23:17 +0530

    Encapsulation

--- src/UdemyTelesko/corejava/Encapsulation.java
+++ src/UdemyTelesko/corejava/Encapsulation.java
@@ -1,7 +1,27 @@
 package UdemyTelesko.corejava;
 
+class human{
+    private int age;
+    private String name;
+
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public void SetAge(int a){
+        age=a;
+    }
+    public void SetName(String n){
+        name=n;
+    }
+}
 public class Encapsulation {
     public static void main(String[] args) {
-
+human h=new human();
+h.SetAge(10);
+h.SetName("James");
+        System.out.println(h.getAge()+" :"+h.getName());
     }
 }
