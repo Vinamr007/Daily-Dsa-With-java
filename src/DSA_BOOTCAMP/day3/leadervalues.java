ea290ea9b228709ffe44a314650aac9269f56bc3
Vinamr Naik <vinamrnaik206@gmail.com>
2026-06-27 01:03:12 +0530

    dsa problem

---
+++ src/DSA_BOOTCAMP/day3/leadervalues.java
@@ -0,0 +1,20 @@
+package DSA_BOOTCAMP.day3;
+
+public class leadervalues {
+    public static void main(String[] args) {
+        int[] a={16, 17, 4, 3, 5, 2};
+
+        for(int i=0;i<a.length;i++){
+            boolean isleader=true;
+            for(int j=i+1;j<a.length;j++){
+                if(a[j]>a[i]){
+                    isleader=false;
+                    break;
+                }
+            }
+            if(isleader){
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
