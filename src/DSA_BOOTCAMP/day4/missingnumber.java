57ec6594f7ecd758fcc0f529cc7c8dfa8fcdfcec
Vinamr Naik <vinamrnaik206@gmail.com>
2026-06-29 10:19:26 +0530

    first non repeating character

---
+++ src/DSA_BOOTCAMP/day4/missingnumber.java
@@ -0,0 +1,16 @@
+package DSA_BOOTCAMP.day4;
+
+public class missingnumber {
+    public static void main(String[] args) {
+        int[] a={1,2,4,5,6};
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            sum+=a[i];
+        }
+
+        int n=a.length+1;
+        int actual=n*(n+1)/2;
+        int missing=actual-sum;
+        System.out.println(missing);
+    }
+}
---
+++ src/DSA_BOOTCAMP/day5/firstnonRepeatingCharacter.java
@@ -0,0 +1,22 @@
+package DSA_BOOTCAMP.day5;
+
+public class firstnonRepeatingCharacter {
+    public static void main(String[] args) {
+        int[] a={9, 4, 9, 6, 7, 4};
+       for(int i=0;i<a.length;i++){
+           int count=0;
+           for(int j=0;j<a.length;j++){
+               if(a[i]==a[j]){
+                   count++;
+               }
+
+
+               }
+           if(count== 1){
+
+               System.out.println(a[i]);
+               break;
+           }
+       }
+    }
+}
