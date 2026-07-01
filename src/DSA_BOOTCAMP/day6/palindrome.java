226f10dd3900b58349f764ac6d52aaf2006a7e6c
Vinamr Naik <vinamrnaik206@gmail.com>
2026-07-01 23:51:45 +0530

    palindrome number

---
+++ src/DSA_BOOTCAMP/day6/palindrome.java
@@ -0,0 +1,28 @@
+package DSA_BOOTCAMP.day6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        int n1=n;
+int st=0, sum=0, rem=0,rev=0;
+while(n>0){
+    rem=n%10;
+    rev=rev*10+rem;
+    n=n/10;
+}
+if(rev==n1){
+    System.out.println("the number is palindrome");
+}else {
+    System.out.println("the number is not palindrome");
+}
+
+
+
+
+    }
+}
