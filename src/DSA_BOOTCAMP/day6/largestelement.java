cdb41140f01161e021dfeefa15d93e1b24d49751
Vinamr Naik <vinamrnaik206@gmail.com>
2026-07-02 23:01:41 +0530

    lergest elemt

---
+++ src/DSA_BOOTCAMP/day6/largestelement.java
@@ -0,0 +1,16 @@
+package DSA_BOOTCAMP.day6;
+
+public class largestelement
+{
+    public static void main(String[] args) {
+        int[] a={12, 45, 7, 89, 23};
+        int max=a[0];
+        for(int i=0;i<a.length;i++){
+            if(a[i]>max){
+                max=a[i];
+
+            }
+        }
+        System.out.println(max);
+    }
+}
