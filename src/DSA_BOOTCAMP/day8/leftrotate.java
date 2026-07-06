20494368fbb05de1cfe7ab9fba49d12d700e92b5
Vinamr Naik <vinamrnaik206@gmail.com>
2026-07-07 00:30:44 +0530

    left rotate

---
+++ src/DSA_BOOTCAMP/day8/leftrotate.java
@@ -0,0 +1,37 @@
+package DSA_BOOTCAMP.day8;
+
+import java.util.Scanner;
+
+public class leftrotate {
+
+        public static void reverse(int[] a,int st,int end){
+            while(st<end){
+                int temp=a[st];
+                a[st]=a[end];
+                a[end]=temp;
+                st++;
+                end--;
+            }
+        }
+
+        public static void leftrotate(int[] a,int k){
+            k=k%a.length;
+            reverse(a, 0, k - 1);
+            reverse(a, k, a.length - 1);
+            reverse(a, 0, a.length - 1);
+
+
+
+            for(int i=0;i<a.length;i++){
+                System.out.print(a[i]+" ");
+            }
+        }
+
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5};
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter k");
+        int k=sc.nextInt();
+        leftrotate(a,k);
+    }
+}
