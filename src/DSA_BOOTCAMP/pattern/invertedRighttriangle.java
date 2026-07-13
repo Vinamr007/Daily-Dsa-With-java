9c097b27c473cfbcdb23ae3f543bc919b19b055b
Vinamr Naik <vinamrnaik206@gmail.com>
2026-07-13 12:13:02 +0530

    inverted triangle

---
+++ src/DSA_BOOTCAMP/pattern/invertedRighttriangle.java
@@ -0,0 +1,12 @@
+package DSA_BOOTCAMP.pattern;
+
+public class invertedRighttriangle {
+    public static void main(String[] args) {
+for(int i=1;i<=5;i++){
+    for(int j=5;j>=i;j--){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+    }
+}
