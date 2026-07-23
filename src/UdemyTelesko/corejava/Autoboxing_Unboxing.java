200e1d8228314dd8d6de724a2d4c3468fdf0f9f3
Vinamr Naik <vinamrnaik206@gmail.com>
2026-07-23 14:47:18 +0530

    autoboxing unexpeted behaviour

---
+++ src/UdemyTelesko/corejava/Autoboxing_Unboxing.java
@@ -0,0 +1,15 @@
+package UdemyTelesko.corejava;
+
+public class Autoboxing_Unboxing {
+    public static void main(String[] args) {
+        Integer a = 100;   // Autoboxing
+        Integer b = 100;   // Autoboxing
+
+        System.out.println(a == b);
+
+        Integer x = 200;
+        Integer y = 200;
+
+        System.out.println(x == y);
+    }
+}
