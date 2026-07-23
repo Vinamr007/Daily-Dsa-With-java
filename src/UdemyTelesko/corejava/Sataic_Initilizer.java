0ece6105f2b908ef4ebc6ffbe29d78f651d4ca28
Vinamr Naik <vinamrnaik206@gmail.com>
2026-07-23 11:28:51 +0530

    static initilizer

---
+++ src/UdemyTelesko/corejava/Sataic_Initilizer.java
@@ -0,0 +1,10 @@
+package UdemyTelesko.corejava;
+
+public class Sataic_Initilizer {
+    static{
+        System.out.println("Sataic_Initilizer");
+    }
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+}
