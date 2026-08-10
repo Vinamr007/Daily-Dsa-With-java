df0f6f02f43e968cb6a2d1441a36e101698da38f
Vinamr Naik <vinamrnaik206@gmail.com>
2026-08-10 22:49:16 +0530

    frequency of element

---
+++ src/DSA_BOOTCAMP/Array/Day14/FrequencyOfElement.java
@@ -0,0 +1,32 @@
+package DSA_BOOTCAMP.Array.Day14;
+
+public class FrequencyOfElement {
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 2, 3, 1, 4, 2};
+
+        boolean[] visited = new boolean[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+
+            // Skip if this element is already counted
+            if (visited[i]) {
+                continue;
+            }
+
+            int count = 1;
+
+            // Compare with remaining elements
+            for (int j = i + 1; j < a.length; j++) {
+
+                if (a[i] == a[j]) {
+                    count++;
+
+                    // Mark duplicate as visited
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(a[i] + " -> " + count);
+        }
+    }
