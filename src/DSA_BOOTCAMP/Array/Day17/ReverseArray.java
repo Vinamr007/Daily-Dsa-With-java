9b85f197fd13a68d1c7682798552a15b92379303
Vinamr Naik <vinamrnaik206@gmail.com>
2026-08-29 20:40:53 +0530

    reverse array

---
+++ src/DSA_BOOTCAMP/Array/Day17/ReverseArray.java
@@ -0,0 +1,24 @@
+package DSA_BOOTCAMP.Array.Day17;
+
+public class ReverseArray {
+    public static void reverse(int[] a){
+        int st=0;
+        int end=a.length-1;
+
+        while(st<end){
+            int temp=a[st];
+            a[st]=a[end];
+            a[end]=temp;
+            end--;
+            st++;
+        }
+        for(int s: a){
+            System.out.print(s+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5,6};
+        reverse(a);
+    }
+}
