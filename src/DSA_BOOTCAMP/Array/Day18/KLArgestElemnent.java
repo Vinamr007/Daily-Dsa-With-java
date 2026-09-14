9d22f3f7f9f346bbac7470d36e0f3c77807832d0
Vinamr Naik <vinamrnaik206@gmail.com>
2026-09-14 20:00:31 +0530

    Solve Kth Largest Element

---
+++ src/DSA_BOOTCAMP/Array/Day18/KLArgestElemnent.java
@@ -0,0 +1,12 @@
+package DSA_BOOTCAMP.Array.Day18;
+
+import java.util.Arrays;
+
+public class KLArgestElemnent {
+    public static void main(String[] args) {
+        int[] a={3, 2, 1, 5, 6, 4};
+
+        Arrays.sort(a);
+        System.out.println(a.length-1);
+    }
+}
